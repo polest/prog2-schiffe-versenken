c4bdad644f2fa0fb86c1ca3c9cb2699788a3cfa0
emal145 <emal145@hotmail.com>
2015-04-13 18:15:50 +0200

    eclipse Battelship Projekt
    Projekt angelegt
    Klassen für die Schiffe programmiert
    UML des Programms

---
+++ Schiffe_Versenken/src/Ships/Submarine.java
@@ -0,0 +1,13 @@
+package Ships;
+
+public class Submarine extends Ship{
+
+	/**
+	 * Destroyer Ship Konstruktor erbt von Ship
+	 * übergibt der Oberklasse die Paramter 
+	 * int shipSize = 3, int reloadTime = 1, int shootArea = 1
+	 */
+	public Submarine() {
+		super(2, 1, 1);
+	}
+}
